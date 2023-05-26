54043dd2c2cf15a95644b21c282998c884e40c6b
3rWaZzZa <3rWaZzZa@DESKTOP-K0UNRD6>
2023-05-26 15:38:45 +0200

    rama2

---
+++ Proyecto2/src/paquete/OtraClase.java
@@ -0,0 +1,8 @@
+package paquete;
+
+public class OtraClase {
+	public static void main(String[] args) {
+        System.out.println("Mensaje 3");
+        System.out.println("Nuevo mensaje 4");
+    }
+}
